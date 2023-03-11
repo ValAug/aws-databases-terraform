46a9dd7a6666a5b9fe01d0d816dc4cc051176745
ValAug <valdiviaaugusto9@gmail.com>
2023-03-11 16:33:37 -0500

    iot core created it

--- iot.tf
+++ iot.tf
@@ -1,23 +1,22 @@
 #--iot/main--
 
-# Create an AWS IoT Core
 resource "aws_iot_thing" "iot_core" {
   name = "your-iot-core-thing"
 }
 
-# Create a certificate for the AWS IoT Core
+
 resource "aws_iot_certificate" "iot_certificate" {
   active = true
 }
 
-# Attach the certificate to the AWS IoT Core
+
 resource "aws_iot_thing_principal_attachment" "iot_attachment" {
   principal = aws_iot_certificate.iot_certificate.arn
   thing = aws_iot_thing.iot_core.name
 }
 
 
-# Create an AWS IoT rule
+
 resource "aws_iot_topic_rule" "iot_rule" {
   name = "myiotrule"
   sql = "SELECT * FROM 'iot-core-topic'"
@@ -26,7 +25,7 @@ resource "aws_iot_topic_rule" "iot_rule" {
   description = "Send data from the AWS IoT Core to AWS Timestream"
 }
 
-# Create an AWS Timestream policy
+
 resource "aws_iam_policy" "iot_timestream_policy" {
   name = "iot-timestream-policy"
   policy = jsonencode({
@@ -45,7 +44,6 @@ resource "aws_iam_policy" "iot_timestream_policy" {
   })
 }
 
-# Attach the AWS Timestream policy to the certificate
 resource "aws_iot_policy_attachment" "iot_policy_attachment" {
   policy = aws_iam_policy.iot_timestream_policy.name
   target = aws_iot_certificate.iot_certificate.arn
